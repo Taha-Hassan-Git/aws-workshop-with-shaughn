5bd8a1302b2b2ede4848737b2550073678a709b8
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-05-31 11:57:55 +0100

    Initial commit

---
+++ test/aws-workshop-with-shaughn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsWorkshopWithShaughn from '../lib/aws-workshop-with-shaughn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-workshop-with-shaughn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsWorkshopWithShaughn.AwsWorkshopWithShaughnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
